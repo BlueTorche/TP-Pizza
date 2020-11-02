05b7a4380cd34a3f31ba38e189f7bd366b15ef21
Florian Dubois <florian.dubois@me.com>
2020-11-02 13:55:08 +0100

    state en cours d'implémebtation reflexion de classes upload

--- src/main/java/be/ac/umons/Class/State/Running.java
+++ src/main/java/be/ac/umons/Class/State/Running.java
@@ -1,9 +1,9 @@
 package be.ac.umons.Class.State;
 
 import be.ac.umons.Class.Pizza.Pizza;
-import java.lang.reflect.*;
+import org.reflections.Reflections;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Running implements State{
 
@@ -16,14 +16,47 @@ public class Running implements State{
         Scanner s = new Scanner(System.in);
         String answ = s.nextLine();
 
-        if(answ == "C") context.carte(context);
-        if(answ == "O") this.option(context);
+        if(answ.equals("C")) context.carte(context);
+        if(answ.equals("O")) this.option(context);
         else System.out.println("Bye");
     }
 
     @Override
-    public void carte(StateContext context) {
+    public void carte(StateContext context){
+        ArrayList<Pizza> carte = new ArrayList<>();
+        Reflections reflections = new Reflections("be.ac.umons.Class.Pizza");
 
+        Set<Class<? extends Pizza>> classSet = reflections.getSubTypesOf(Pizza.class);
+        for(Class<? extends Pizza> clazz : classSet) {
+            try {
+                Pizza pizza = clazz.newInstance();
+                carte.add(pizza);
+            } catch (IllegalAccessException | InstantiationException e) {
+                e.printStackTrace();
+            }
+        }
+        ListIterator<Pizza> it = carte.listIterator();
+        boolean run = true;
+
+        while(run){
+            System.out.println("Sélectioné:");
+            it.toString();
+            System.out.println("(C)ommander, (P)récédent, (S)uivant,(M)enu,(Q)uitter");
+
+            Scanner s = new Scanner(System.in);
+            String answ = s.nextLine();
+
+            if(answ.equals("C")) context.commande(context,it.next());
+            if(answ.equals("P")){
+                if(!it.hasPrevious()) while(it.hasNext()) it.next();
+                else it.previous();
+            }
+            if(answ.equals("S")){
+                if(!it.hasNext()) while(it.hasPrevious()) it.previous();
+                else it.next();
+            }
+            if(answ.equals("M")) context.menu(context);
+        }
     }
 
     @Override
@@ -37,6 +70,6 @@ public class Running implements State{
         Scanner s = new Scanner(System.in);
         String answ = s.nextLine();
 
-        if(answ == "R") context.menu(context);
+        if(answ.equals("R")) context.menu(context);
     }
 }
